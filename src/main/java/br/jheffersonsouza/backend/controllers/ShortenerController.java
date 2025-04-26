5e60b82b51afa59b8289890e85b9eff092cbd244
Jhefferson Souza <188925448+jheffersonsouza@users.noreply.github.com>
2025-04-26 17:05:23 -0300

    Adding docs to controller.

--- src/main/java/br/jheffersonsouza/backend/controllers/ShortenerController.java
+++ src/main/java/br/jheffersonsouza/backend/controllers/ShortenerController.java
@@ -1,6 +1,8 @@
 package br.jheffersonsouza.backend.controllers;
 
 import br.jheffersonsouza.backend.service.UrlShortenerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +15,21 @@ public class ShortenerController {
     UrlShortenerService urlShortenerService;
 
     @PostMapping("/shorten")
+    @Operation(summary = "Shorten an URL.",
+    responses = {
+            @ApiResponse(responseCode = "201")
+    })
     public ResponseEntity<String> shorten(@RequestParam String url) {
         return ResponseEntity
                 .created(URI.create(urlShortenerService.shorten(url)))
                 .build();
     }
     @GetMapping("/{shortenedUrl}")
+    @Operation(summary = "Get a URL from a shortened one.",
+    responses = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", description = "URL not found")
+    })
     public ResponseEntity<String> getShortenedUrl(@PathVariable String shortenedUrl) {
        shortenedUrl = urlShortenerService.shorten(shortenedUrl);
        if (shortenedUrl == null) {
